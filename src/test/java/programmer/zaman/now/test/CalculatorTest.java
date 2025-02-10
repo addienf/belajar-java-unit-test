f0269693df93e1cb57460f4fb959c8f84842cf37
addienaf <addienaf2001@gmail.com>
2025-02-10 23:18:21 +0700

    Pelajaran 1 Jam Pertama

--- src/test/java/programmer/zaman/now/test/CalculatorTest.java
+++ src/test/java/programmer/zaman/now/test/CalculatorTest.java
@@ -1,33 +1,68 @@
 package programmer.zaman.now.test;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.opentest4j.TestAbortedException;
+import programmer.zaman.now.test.generator.SimpleDisplayNameGenerator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("Test Calculator")
+@DisplayNameGeneration(value = SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("Before Unit Test");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("After Unit Test");
+    }
+
     @Test
-    @DisplayName("Test Function Calculator.add(Integer, Integer")
     public void testAddSuccess(){
         var result = calculator.add(10, 10);
         assertEquals(20, result);
     }
 
     @Test
-    @DisplayName("Test Success Function Calculator.devide(Integer, Integer)")
     public void testDevideSuccess(){
         var result = calculator.devide(100, 10);
         assertEquals(10, result);
     }
 
     @Test
-    @DisplayName("Test Fail Function Calculator.devide(Integer, Integer)")
-    public void testDevideError(){
+    public void testDevideFailed(){
         assertThrows(IllegalArgumentException.class, () ->{
            calculator.devide(100, 0);
         });
     }
+
+    @Test
+    @Disabled
+    public void testComingSoon(){
+
+    }
+
+    @Test
+    public void testAborted(){
+        var profile = System.getenv("PROFILE");
+        if (!"DEV".equals(profile)){
+            throw new TestAbortedException("Test dibatalkan karena bukan DEV");
+        }
+    }
+
+    //Unit test untuk DEV
 }
